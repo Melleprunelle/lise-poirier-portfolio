b0075dde341e8cc88b14aceb9307de3158303699
facerennes <facerennes>
2017-06-20 17:35:18 +0200

    Mise en place portfolio

--- js/script.js
+++ js/script.js
@@ -1,60 +1,14 @@
-$('#css').on('inview', function (event, isInView) {
+//CAT
+$('.cat').on('inview', function (event, isInView) {
     if (isInView) {
         $(this).css({
-            "width": "23vw",
-            "transition": "4s"
+            margin: "-5px auto 0",
+            "transition": "0.5s"
         })
     } else {
         $(this).css({
-            "width": "12vw"
-        })
-    }
-});
-$('#html').on('inview', function (event, isInView) {
-    if (isInView) {
-        $(this).css({
-            "width": "21vw",
-            "transition": "4s"
-        })
-    } else {
-        $(this).css({
-            "width": "12vw"
-        })
-    }
-});
-$('#js').on('inview', function (event, isInView) {
-    if (isInView) {
-        $(this).css({
-            "width": "15vw",
-            "transition": "4s"
-        })
-    } else {
-        $(this).css({
-            "width": "12vw"
-        })
-    }
-});
-$('#bootstrap').on('inview', function (event, isInView) {
-    if (isInView) {
-        $(this).css({
-            "width": "18vw",
-            "transition": "4s"
-        })
-    } else {
-        $(this).css({
-            "width": "12vw"
-        })
-    }
-});
-$('#autre').on('inview', function (event, isInView) {
-    if (isInView) {
-        $(this).css({
-            "width": "25vw",
-            "transition": "4s"
-        })
-    } else {
-        $(this).css({
-            "width": "12vw"
+            margin: "200px auto 0",
+            "transition": "0.5s"
         })
     }
 });
